79261aecc75282443b723ece99e2484c02f2922f
Yonggang Liu <yonggang@yonggang-laptop.(none)>
2011-06-29 11:32:09 -0400

    Version 0.15

--- omnetpp/scheduler/DSschedulerspecs.h
+++
@@ -1,6 +0,0 @@
-#define SCH_NJ_DELAY 0 // process time at the scheduler for new jobs
-#define SCH_FJ_DELAY 0 // process time at the scheduler for finished jobs
-
-#define SCH_DEGREE 4
-
-#define DSSCH_ID_BASE (C_ID_BASE+C_TOTAL+DS_TOTAL*3)
