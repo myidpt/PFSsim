8e712eca494a987eff4b28b24c0e1bbf45120631
Vivien-Michel <vivien.michel@e.ujf-grenoble.fr>
2015-07-14 15:07:43 -0300

    modification of script gen_input : number of client, server, stripe size... can be modified when the script is call with the proper arguments  --client=... In order to launch different simulations and don't open the script to change parameters

--- omnetpp/gen_input.sh
+++ omnetpp/gen_input.sh
@@ -3,11 +3,11 @@
 CLT_NUM=16
 TRC_PER_CLT=2
 SVR_NUM=8
-FILE_NUM=$(echo "$CLT_NUM*$TRC_PER_CLT" | bc)
 FILE_SIZE=536870912
 REQ_SIZE=1048576
 READ_FLAG=1
 STRIPE_SIZE=64K
+FILE_PER_PROCESS=1
 
 for WORD in "$@";
 do
@@ -22,17 +22,55 @@ do
     --help)
       echo "Options:"
       echo "--python-prefix= # Provide the path to the python binary directory."
+      echo "--client= # Provide the number of client"
+      echo "--trace-num= # Provide the number of trace per client"
+      echo "--server= # Provide the number of server"
+      echo "--file-size= # Provide the size of file (in byte)"
+      echo "--req-size= # Provide the size of each quantity of data asks by client in one time (in byte)"
+      echo "--read= # 0 for write file/1 read file"
+      echo "--stripe-size= # Provide the stripe-size : Don't forget the unit at the end : K for KB, M for MB"
+      echo "--uniq-shared-file= #Provide one file shared between clients"
       echo "--help           # Print this information."
       exit ;;
+    --client=*)
+		CLT_NUM=${WORD:9}
+		shift ;;
+    --trace-num=*)
+		TRC_PER_CLT=${WORD:12}
+		shift ;;
+	--server=*)
+		SVR_NUM=${WORD:9}
+		shift ;;
+	--file-size=*)
+		FILE_SIZE=${WORD:12}
+		shift ;;
+	--req-size=*)
+		REQ_SIZE=${WORD:11}
+		shift ;;
+	--read=*)
+		READ_FLAG=${WORD:7}
+		shift ;;
+	--stripe-size=*)
+		STRIPE_SIZE=${WORD:14}
+		shift ;;
+	--uniq-shared-file)
+		FILE_PER_PROCESS=0
+		shift ;;
     *)
       echo "Unrecognized argument: " $WORD
       exit ;;
   esac
 done
 
+if [ $FILE_PER_PROCESS -eq 1 ];
+then
+	FILE_NUM=$(echo "$CLT_NUM*$TRC_PER_CLT" | bc)
+else
+	FILE_NUM=1
+fi
 
 cd input/synthetic
-${PYTHON_PREFIX}python ./gen.py $CLT_NUM $TRC_PER_CLT $FILE_SIZE $REQ_SIZE $READ_FLAG
+${PYTHON_PREFIX}python ./gen.py $CLT_NUM $TRC_PER_CLT $FILE_SIZE $REQ_SIZE $READ_FLAG $FILE_PER_PROCESS
 cd ../..
 
 cd config/pfslayout/synthetic/even-dist
